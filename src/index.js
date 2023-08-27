8632c41e5158cda53ad805fbc11055b85c3216fa
Nester44 <nesterov.mail4@gmail.com>
2023-08-27 12:59:11 +0200

    add port in webhook config

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000
 
 if (process.env.NODE_ENV === 'production') {
   createServer(
-    await bot.createWebhook({ domain: process.env.WEBHOOK_DOMAIN }),
+    await bot.createWebhook({ domain: process.env.WEBHOOK_DOMAIN, port: PORT }),
   ).listen(PORT, () => {
     console.log(`Bot is running on production mode at port ${PORT}`)
   })
