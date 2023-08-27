0ac31a4cbd2fcdfdc810a962e8f410ea219bbdf2
Nester44 <nesterov.mail4@gmail.com>
2023-08-27 18:28:04 +0200

    refactored

--- src/dialogs/default.js
+++ src/dialogs/default.js
@@ -1,8 +1,17 @@
-export const getDefaultDialog = (text) => [
+const defaultDialog = [
   {
-    role: 'user',
+    role: 'system',
     content:
-      'Answer concise and short, but include all of the neccesary details.' +
-      text,
+      'You are @nesterkrutbot. You were created by Nikita Nesterov. You have to answer questions.',
   },
 ]
+
+export const getDefaultDialog = (text) =>
+  defaultDialog.concat([
+    {
+      role: 'user',
+      content:
+        'Answer concise and short, but include all of the neccesary details.' +
+        text,
+    },
+  ])
