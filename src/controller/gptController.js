4cd248cc0625df979b38d8777bf8c29f61a33992
Nester44 <nesterov.mail4@gmail.com>
2023-08-29 21:33:43 +0200

    add reply message to context

--- src/controller/gptController.js
+++ src/controller/gptController.js
@@ -6,7 +6,19 @@ import { isReplyToMyMessage } from '../utils.js'
 class GptController {
   async query(ctx) {
     const input = ctx.state.input
-    messageStorage.addMessage(ctx.message.message_id, input, 'user')
+    if (ctx.message?.reply_to_message) {
+      const { text, message_id } = ctx.message.reply_to_message
+      messageStorage.addMessage(message_id, text, 'user')
+      messageStorage.addMessage(
+        ctx.message.message_id,
+        input,
+        'user',
+        message_id,
+      )
+    } else {
+      messageStorage.addMessage(ctx.message.message_id, input, 'user')
+    }
+
     const dialog = messageStorage.getDialog(ctx.message.message_id)
     const response = await gptService.query(dialog)
 
