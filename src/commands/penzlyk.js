2a8f5bd67fb0b7da54a2f7500d38fff999de6684
Nikita Nesterov <nesterov.mail4@gmail.com>
2023-09-07 14:56:43 +0000

    hotfix/add trim to the messages

--- src/commands/penzlyk.js
+++ src/commands/penzlyk.js
@@ -1,7 +1,7 @@
 import { generateImage } from "../service/imageService.js"
 
 const penzlyk = async(ctx) => {
-    const input = ctx.message.text.replace('/penzlyk', '')
+    const input = ctx.message.text.replace('/penzlyk', '').trim()
 
     if (!input) ctx.reply('Напиши ти трясця інструкцію', { reply_to_message_id: ctx.message.message_id,})    
     try {
@@ -9,7 +9,7 @@ const penzlyk = async(ctx) => {
         ctx.replyWithPhoto({ url: imageUrl }, { reply_to_message_id: ctx.message.message_id,});
 
     } catch (error) {
-        ctx.reply(error.message)
+        ctx.reply(error.message, { reply_to_message_id: ctx.message.message_id,})
         console.error(error.message)        
     }
 }
