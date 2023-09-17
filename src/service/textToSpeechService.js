0910831c40610e2553f6dc1ddde104b8b0ba7f47
Nester44 <nesterov.mail4@gmail.com>
2023-09-17 02:04:31 +0200

    add 3 language support

--- src/service/textToSpeechService.js
+++ src/service/textToSpeechService.js
@@ -1,27 +1,41 @@
 // Imports the Google Cloud client library
 import textToSpeech from '@google-cloud/text-to-speech'
+import { franc } from 'franc'
 
 // Creates a client
 const client = new textToSpeech.TextToSpeechClient()
 
+const mapLanguageToConfig = {
+  rus: {
+    languageCode: 'ru-RU',
+    ssmlGender: 'MALE',
+    name: 'ru-RU-Wavenet-D',
+  },
+  eng: {
+    languageCode: 'en-US',
+    name: 'en-US-Neural2-A',
+    ssmlGender: 'MALE',
+  },
+  ukr: {
+    languageCode: 'uk-UA',
+    name: 'uk-UA-Standard-A',
+    ssmlGender: 'FEMALE',
+  },
+}
+
 async function synthesizeVoice(text) {
   // The text to synthesize
-
+  const language = franc(text, { only: ['rus', 'ukr', 'eng'] })
   // Construct the request
   const request = {
     input: { text },
-    // Select the language and SSML voice gender (optional)
-    voice: {
-      languageCode: 'ru-RU',
-      ssmlGender: 'MALE',
-      name: 'ru-RU-Wavenet-D',
-    },
+    voice: mapLanguageToConfig[language],
     // select the type of audio encoding
     audioConfig: { audioEncoding: 'MP3', pitch: -4, speed: 1.2 },
   }
 
   // Performs the text-to-speech request
-  const [response] = client.synthesizeSpeech(request)
+  const [response] = await client.synthesizeSpeech(request)
   return response.audioContent
 }
 
