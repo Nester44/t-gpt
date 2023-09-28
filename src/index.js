0bd9d61d634dca2af3cfccd5f724216d613493f8
Nikita Nesterov <nesterov.mail4@gmail.com>
2023-09-28 10:56:08 +0200

    return commented code

--- src/index.js
+++ src/index.js
@@ -3,26 +3,16 @@ import bot from './bot.js'
 
 const PORT = process.env.PORT || 3000
 
-createServer(
-	await bot.createWebhook({
-		domain: process.env.WEBHOOK_DOMAIN,
-		port: PORT,
-	}),
-).listen(PORT, () => {
-	console.log(`Bot is running on production mode at port ${PORT}`)
-	console.log('NODE_ENV', process.env.NODE_ENV)
-})
-
-// if (process.env.NODE_ENV === 'production') {
-// 	createServer(
-// 		await bot.createWebhook({
-// 			domain: process.env.WEBHOOK_DOMAIN,
-// 			port: PORT,
-// 		}),
-// 	).listen(PORT, () => {
-// 		console.log(`Bot is running on production mode at port ${PORT}`)
-// 	})
-// } else {
-// 	bot.launch()
-// 	console.log('Bot is running on development mode')
-// }
+if (process.env.NODE_ENV === 'production') {
+  createServer(
+    await bot.createWebhook({
+      domain: process.env.WEBHOOK_DOMAIN,
+      port: PORT,
+    }),
+  ).listen(PORT, () => {
+    console.log(`Bot is running on production mode at port ${PORT}`)
+  })
+} else {
+  bot.launch()
+  console.log('Bot is running on development mode')
+}
