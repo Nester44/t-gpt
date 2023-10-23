8f9e546ec3749c71bf18d537c89a5b0d6e1052c6
Nikita Nesterov <nesterov.mail4@gmail.com>
2023-10-23 14:19:29 +0200

    enhancement: remove voice

--- src/controller/gptController.js
+++ src/controller/gptController.js
@@ -2,51 +2,44 @@
 import synthesizeVoice from '../service/textToSpeechService.js'
 
 class GptController {
-	constructor(gptService) {
-		/** @type {import('../service/gptService.js').default} */
-		this.gptService = gptService
-	}
-	query = async (ctx) => {
-		const input = ctx.state.input
+  constructor(gptService) {
+    /** @type {import('../service/gptService.js').default} */
+    this.gptService = gptService
+  }
+  query = async (ctx) => {
+    const input = ctx.state.input
 
-		const messageId = ctx.message.message_id
-		const replyToId = ctx.message?.reply_to_message?.message_id
+    const messageId = ctx.message.message_id
+    const replyToId = ctx.message?.reply_to_message?.message_id
 
-		const [response, handleSentMessage] = await this.gptService.query(
-			input,
-			messageId,
-			replyToId,
-		)
+    const [response, handleSentMessage] = await this.gptService.query(
+      input,
+      messageId,
+      replyToId,
+    )
 
-		// Telegram has a limit of 1000 characters per message with voice
-		const slicedResponse = response.slice(0, 1000)
+    const sentMessage = await ctx.reply(response, {
+      reply_to_message_id: replyToId,
+    })
 
-		const voice = await synthesizeVoice(slicedResponse)
+    handleSentMessage(sentMessage.message_id)
+  }
 
-		const sentMessage = await ctx.replyWithVoice(voice, {
-			// eslint-disable-next-line camelcase
-			reply_to_message_id: messageId,
-			caption: slicedResponse,
-		})
+  switchMode = (ctx) => {
+    const mode = this.gptService.switchMode()
+    console.log('Mode switched to', mode)
+    ctx.reply('Режим успешно изменен на ' + mode)
+  }
 
-		handleSentMessage(sentMessage.message_id)
-	}
+  penzlyk = async (ctx) => {
+    const input = ctx.state.input
 
-	switchMode = (ctx) => {
-		const mode = this.gptService.switchMode()
-		console.log('Mode switched to', mode)
-		ctx.reply('Режим успешно изменен на ' + mode)
-	}
-
-	penzlyk = async (ctx) => {
-		const input = ctx.state.input
-
-		const imageUrl = await this.gptService.generateImage(input)
-		ctx.replyWithPhoto(
-			{ url: imageUrl },
-			{ reply_to_message_id: ctx.message.message_id },
-		)
-	}
+    const imageUrl = await this.gptService.generateImage(input)
+    ctx.replyWithPhoto(
+      { url: imageUrl },
+      { reply_to_message_id: ctx.message.message_id },
+    )
+  }
 }
 
 export default GptController
